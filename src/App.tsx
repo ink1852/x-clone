4ef41a045e8e05f30edd03f165e436a341c21d32
ink1852 <hjgim07@gmail.com>
2026-08-16 00:00:50 +0900

    modified router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/layout";
 import Home from "./routes/Home";
 import Profile from "./routes/Profile";
 import Login from "./routes/Login";
 import CreateAccount from "./routes/Create-account";
-
+import { reset } from "styled-reset";
+import { createGlobalStyle } from "styled-components";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -30,4 +31,22 @@ const router = createBrowserRouter([
   },
 ]);
 
-export default router;
+const GrobalStyles = createGlobalStyle`
+  ${reset};
+  body{
+    background-color: #17181a;
+    color: white;
+    font-family: "Saira Stencil", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 400;
+  }
+`;
+function App() {
+  return (
+    <>
+      <GrobalStyles />
+      <RouterProvider router={router} />
+    </>
+  );
+}
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import router from "./App";
+
 import { HelmetProvider } from "react-helmet-async";
-import { GrobalStyles } from "./style/global-style";
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HelmetProvider>
-      <GrobalStyles />
-      <RouterProvider router={router} />
+      <App />
     </HelmetProvider>
   </StrictMode>,
 );
