ba225c370a3a270ae8bfef26644f82cb3b12524c
ink1852 <hjgim07@gmail.com>
2026-08-16 00:30:53 +0900

    add firebase

---
+++ src/firebase.tsx
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDUPRyybvLSnRHWGyykkF8FP_ObQB43IEA",
+  authDomain: "x-clone-bfec2.firebaseapp.com",
+  projectId: "x-clone-bfec2",
+  storageBucket: "x-clone-bfec2.firebasestorage.app",
+  messagingSenderId: "715295284819",
+  appId: "1:715295284819:web:fb1791803907a77fdff0a6",
+};
+
+const app = initializeApp(firebaseConfig);
